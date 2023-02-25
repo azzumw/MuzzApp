48615b74d76d8514f62939abd06883d850c76789
azzumwaqar <azzumwaqar@gmail.com>
2023-02-25 00:37:42 +0000

    chore: clean - ready to implement logic

--- app/src/androidTest/java/com/example/muzzapp/MainActivityTests.kt
+++ app/src/androidTest/java/com/example/muzzapp/MainActivityTests.kt
@@ -6,10 +6,8 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.appcompat.widget.Toolbar
-
 import org.junit.Test
 import org.junit.runner.RunWith
-
 import org.junit.Assert.*
 
 /**

d1e2ac3e702a31f24998e73bce3463945e0a1030
azzumwaqar <azzumwaqar@gmail.com>
2023-03-01 10:27:00 +0000

    chore: clean

--- app/src/main/java/com/example/muzzapp/adapter/BindingAdapters.kt
+++ app/src/main/java/com/example/muzzapp/adapter/BindingAdapters.kt
@@ -10,8 +10,6 @@ fun bindChatRecyclerView(recyclerView: RecyclerView,messages:List<Message>?){
     val adapter = recyclerView.adapter as ChatAdapter
     messages?.let {
         adapter.messages = it
-        Log.e("Bin-adapter: ",adapter.itemCount.toString())
-        Log.e("Bin-adapter: Msg Size: ",messages.size.toString())
         recyclerView.scrollToPosition(it.lastIndex)
     }
 }

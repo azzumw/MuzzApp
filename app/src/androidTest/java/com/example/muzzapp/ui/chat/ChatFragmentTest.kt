8e69f48769bb5903f9f085b93cdcfe276dc8ddb4
azzumwaqar <azzumwaqar@gmail.com>
2023-03-05 21:41:22 +0000

    tests: FakeRepository.kt added to androidTests

--- app/src/androidTest/java/com/example/muzzapp/ui/chat/ChatFragmentTest.kt
+++ app/src/androidTest/java/com/example/muzzapp/ui/chat/ChatFragmentTest.kt
@@ -3,7 +3,10 @@ package com.example.muzzapp.ui.chat
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
+import com.example.muzzapp.repository.FakeRepository
+import com.example.muzzapp.repository.Repository
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -11,9 +14,18 @@ import org.junit.runner.RunWith
 @MediumTest
 class ChatFragmentTest {
 
+    private lateinit var repository: Repository
+
+    @Before
+    fun setUp() {
+        repository = FakeRepository()
+    }
+
     @Test
     fun launchFragment() {
+
         launchFragmentInContainer<ChatFragment>(null, com.example.muzzapp.R.style.Theme_MuzzApp)
+
 
     }
 

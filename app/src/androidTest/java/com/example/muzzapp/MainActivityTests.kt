ef13d2cef62c48e60f99fff746179223f9a594ce
azzumwaqar <azzumwaqar@gmail.com>
2023-03-05 23:18:24 +0000

    test: MainActivityTests.kt passing

--- app/src/androidTest/java/com/example/muzzapp/MainActivityTests.kt
+++ app/src/androidTest/java/com/example/muzzapp/MainActivityTests.kt
@@ -1,5 +1,6 @@
 package com.example.muzzapp
 
+import android.os.SystemClock
 import androidx.test.core.app.ActivityScenario.launch
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -7,11 +8,17 @@ import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.appcompat.widget.Toolbar
 import androidx.test.espresso.action.ViewActions.*
-import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
-import org.hamcrest.CoreMatchers.allOf
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.UiDevice
+import com.example.muzzapp.util.ANIMATION_OFF
+import com.example.muzzapp.util.TRANS_ANIMATION_OFF
+import com.example.muzzapp.util.WIN_ANIMATION_OFF
+import org.junit.After
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.BeforeClass
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,6 +27,34 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class MainActivityTests {
+
+    companion object {
+        private lateinit var uiDevice: UiDevice
+
+        //waiting times
+        private const val ONE_SEC = 1000L
+
+        @BeforeClass
+        @JvmStatic
+        fun setDevicePreferences() {
+            //set up animations settings = off
+            uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+            uiDevice.executeShellCommand(ANIMATION_OFF)
+            uiDevice.executeShellCommand(TRANS_ANIMATION_OFF)
+            uiDevice.executeShellCommand(WIN_ANIMATION_OFF)
+        }
+    }
+
+
+    @Before
+    fun setUp() {
+        ServiceLocator.resetRepository()
+    }
+
+    @After
+    fun tearDown() {
+        ServiceLocator.resetRepository()
+    }
 
     @Test
     fun isToolbarTitleDisplayed() {
@@ -31,7 +66,7 @@ class MainActivityTests {
     }
 
     @Test
-    fun typeHello_displaysHelloInChat() {
+    fun sender_user_types_hello_displays_Hello_In_Chat_Window() {
         val scenario = launch(MainActivity::class.java)
 
         onView(withId(R.id.edit_messagebox)).perform(typeText("Hello"), closeSoftKeyboard())
@@ -43,4 +78,26 @@ class MainActivityTests {
 
         scenario.close()
     }
+
+    @Test
+    fun other_user_types_message_displays_the_message() {
+        val scenario = launch(MainActivity::class.java)
+
+        //change to Other User
+        uiDevice.pressMenu()
+
+        onView(withText("Switch User"))
+            .check(matches(isDisplayed()))
+            .perform(click())
+
+        SystemClock.sleep(2000)
+
+
+        onView(withId(R.id.edit_messagebox)).perform(typeText("Hello"), closeSoftKeyboard())
+        onView(withId(R.id.send_button)).perform(click())
+
+        onView(withText("Hello")).check(matches(isDisplayed()))
+        scenario.close()
+    }
+
 }
---
+++ app/src/androidTest/java/com/example/muzzapp/util/DataUtil.kt
@@ -0,0 +1,5 @@
+package com.example.muzzapp.util
+
+const val ANIMATION_OFF = "settings put global animator_duration_scale 0.0"
+const val TRANS_ANIMATION_OFF = "settings put global transition_animation_scale 0.0"
+const val WIN_ANIMATION_OFF = "settings put global window_animation_scale 0.0"
